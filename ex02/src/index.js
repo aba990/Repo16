0a4b5081a836e2f1fb16f19de1cf686aa2d794b3
AnelB <anel.b@live.com>
2022-04-12 18:20:37 +0200

    prvi up

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+class Book {
+    constructor(title) {
+        this._title = title;
+    }
+    get title() {
+        return this._title;
+    }
+    set title(newTitle) {
+        this._title = newTitle;
+    }
+}
+const littlePrince = new Book('The Little Prince'); // Change this line
+console.log(littlePrince.title); // The Little Prince
+littlePrince.title = "Le Petit Prince" // Change this line
+console.log(littlePrince.title); // Le Petit Prince
+module.exports = Book;
